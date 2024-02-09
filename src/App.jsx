b5b595d4b28bad487b61ab4ab9841bee647c8020
Imam Maulana <imammaul2405@gmal.com>
2024-02-09 19:58:31 +0700

    fix: fix devploymet v3

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import {BrowserRouter as Router, Routes, Route} from "react-router-dom"
+import {HashRouter as Router, Routes, Route} from "react-router-dom"
 
 // COMPONENTS
 import Navbar from "./Components/Navbar"
--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react"
 
 // https://vitejs.dev/config/
 export default defineConfig({
+	// base: "/web-app-portfolio/",
 	plugins: [react()],
-	base: "/web-app-portfolio/",
 })
